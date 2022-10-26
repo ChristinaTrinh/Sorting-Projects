1d9ec6a97e207fdc8ca352256ce9ab21a2a63924
Christina <tchristina1607@gmail.com>
2022-10-26 16:01:24 -0400

    Added the new approach of Quick Sort to sort via index.

--- quickSort.java
+++ quickSort.java
@@ -4,9 +4,31 @@ import java.util.*;
 
 public class quickSort {
     public static void main(String[] args) {
-        int[] array = { 37, 2, 6, 10, 4, 3, 14, 89, 77 };
+        // quickSort
+        System.out.println("quickSort: ");
+        // int[] array = { 37, 2, 6, 10, 37, 3, 14, 89, 77 };
+        int array[] = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
         QuickSort(array, 0, array.length);
         System.out.println(Arrays.toString(array));
+        System.out.println();
+
+        // quickSortViaPointers that only switches the index but not the value itself
+
+        int nums[] = { 8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15 };
+        int sorted[] = new int[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            sorted[i] = i;
+
+        System.out.println("quickSortViaPointers: ");
+        System.out.println("Unsorted Nums: ");
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(sorted));
+        System.out.println();
+        quickSortViaPointers(nums, sorted, 0, nums.length);
+        System.out.println("Sorted Nums: ");
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(sorted));
+        System.out.println();
     }
 
     public static void swap(int[] array, int i, int j) {
@@ -42,6 +64,39 @@ public class quickSort {
             // recursively call the method until all the halves are sorted
             QuickSort(array, 0, pivot);
             QuickSort(array, pivot + 1, max);
+
         }
     }
+
+    public static void quickSortViaPointers(int nums[], int sorted[], int min, int max) {
+        if ((max - min) > 0) { // only recursively call the method when there are
+                               // two elements left to sort
+            int pivot = min;
+            int temp = 0;
+
+            // from rightmost to left, if discover a lesser value, then swap
+            for (int i = max - 1; i > pivot; i--) {
+                if (nums[sorted[i]] < nums[sorted[pivot]]) {
+                    swap(sorted, i, pivot);
+                    temp = pivot;
+                    pivot = i;
+
+                    // from left to right, if discover a greater value, then swap
+                    for (int j = temp + 1; j < pivot; j++) {
+                        if (nums[sorted[j]] > nums[sorted[pivot]]) {
+                            swap(sorted, j, pivot);
+                            pivot = j;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // recursively call the method until all the halves are sorted
+            quickSortViaPointers(nums, sorted, 0, pivot);
+            quickSortViaPointers(nums, sorted, pivot + 1, max);
+        }
+
+    }
+
 }
